9c42e9f403648b208f4d0791d31e1450d0da0223
blance97 <lancedinh7@gmail.com>
2024-03-12 14:18:15 -0500

    started work on frontend

--- EchoServerReact/src/components/Sidebar.jsx
+++ EchoServerReact/src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Box, Text, Button, Flex } from '@chakra-ui/react';
 
 const Sidebar = () => {
     return (
         <Box bg="gray.200" p="4" h="100vh" w="250px" position="fixed" left="0" top="0">
             <Text fontWeight="bold" mb="4">Menu</Text>
-            <Button colorScheme="blue">Add Group</Button>
+            <Flex direction="column">
+                <Button colorScheme="blue" mb="2">Add Group</Button>
+                <Button colorScheme="blue">Add API</Button>
+            </Flex>
             {/* Add other sidebar items as needed */}
         </Box>
     );
